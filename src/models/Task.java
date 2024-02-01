db26dc2eaa786fb34b607d934c5a680cafd9cab8
Tatsu-Ishikawa <Tatsu_Ishikawa@sis2.saison.co.jp>
2024-02-01 10:23:42 +0900

    各種サーブレット/jspの実装

--- src/models/Task.java
+++ src/models/Task.java
@@ -13,15 +13,18 @@ import javax.persistence.Table;
 
 @Entity
 @NamedQueries({
-        @NamedQuery(name = "getAllMessages", query = "SELECT m FROM Message AS m ORDER BY m.id DESC"),
-        @NamedQuery(name = "getMessagesCount", query = "SELECT COUNT(m) FROM Message AS m")
+        @NamedQuery(name = "getAllMessages", query = "SELECT m FROM Task AS m ORDER BY m.id DESC"),
+        @NamedQuery(name = "getMessagesCount", query = "SELECT COUNT(m) FROM Task AS m")
 })
-@Table(name = "messages")
-public class Message {
+@Table(name = "task")
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
@@ -38,6 +41,14 @@ public class Message {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getContent() {
